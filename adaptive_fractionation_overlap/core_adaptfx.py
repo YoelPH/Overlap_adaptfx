3dfdf6fabab9d9233974366a3b4653e1c139de25
Yoel Pérez Haas <yoel.perezhaas@usz.ch>
2025-07-14 12:19:56 +0200

    change: changed default values for doses and prior

--- adaptive_fractionation_overlap/core_adaptfx.py
+++ adaptive_fractionation_overlap/core_adaptfx.py
@@ -19,7 +19,7 @@ from .helper_functions import (
 )
 
 
-def adaptive_fractionation_core(fraction: int, volumes: np.ndarray, accumulated_dose: float, steepness: float, number_of_fractions: int = 5, min_dose: float = 7.5, max_dose: float = 9.5, mean_dose:float  = 8, dose_steps: float = 0.25, alpha: float = 1.072846744379587, beta:float = 0.7788684130749829, minimum_benefit = 0):
+def adaptive_fractionation_core(fraction: int, volumes: np.ndarray, accumulated_dose: float, steepness: float, number_of_fractions: int = 5, min_dose: float = 6, max_dose: float = 10, mean_dose:float  = 8, dose_steps: float = 0.25, alpha: float = 1.072846744379587, beta:float = 0.7788684130749829, minimum_benefit = 0):
     """The core function computes the optimal dose for a single fraction.
     The function optimizes the fractionation based on an objective function
     which aims to maximize the tumor coverage, i.e. minimize the dose when
@@ -150,7 +150,7 @@ def adaptive_fractionation_core(fraction: int, volumes: np.ndarray, accumulated_
     return [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]
     
    
-def adaptfx_full(volumes: list, number_of_fractions: int = 5, steepness: float = -0.5, min_dose: float = 7.5, max_dose: float = 9.5, mean_dose: float = 8, dose_steps: float = 0.25, alpha: float = 0.8909285040669036, beta:float = 0.522458969735114, minimum_benefit: float = 0):
+def adaptfx_full(volumes: list, number_of_fractions: int = 5, steepness: float = -0.5, min_dose: float = 6, max_dose: float = 10, mean_dose: float = 8, dose_steps: float = 0.25, alpha: float = 1.072846744379587, beta:float = 0.7788684130749829, minimum_benefit: float = 0):
     """Computes a full adaptive fractionation plan when all overlap volumes are given.
 
     Args:
@@ -183,7 +183,7 @@ def adaptfx_full(volumes: list, number_of_fractions: int = 5, steepness: float =
     return physical_doses, accumulated_doses, total_penalty
 
 
-def precompute_plan(fraction: int, volumes: np.ndarray, accumulated_dose: float, number_of_fractions: int = 5, steepness_penalty: float = -0.5, steepness_benefit: float = -0.1, min_dose: float = 7.5, max_dose: float = 9.5, mean_dose:float  = 8, dose_steps = 0.25, alpha: float = 0.8909285040669036, beta:float = 0.522458969735114, minimum_benefit:float = 0):
+def precompute_plan(fraction: int, volumes: np.ndarray, accumulated_dose: float, number_of_fractions: int = 5, steepness_penalty: float = -0.5, steepness_benefit: float = -0.1, min_dose: float = 6, max_dose: float = 10, mean_dose:float  = 8, dose_steps = 0.25, alpha: float = 1.072846744379587, beta:float = 0.7788684130749829, minimum_benefit:float = 0):
     """Precomputes all possible delivered doses in the next fraction by looping through possible
     observed overlap volumes. Returning a df and two lists with the overlap volumes and
     the respective dose that would be delivered.
